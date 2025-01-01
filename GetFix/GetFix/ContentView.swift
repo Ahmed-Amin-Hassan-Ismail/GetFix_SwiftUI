69b99fb1b1f977707a65f8f6eb6c989f87a88c41
Ahmed Amin Hassan <AhmedAminHassanIsmail@gmail.com>
2025-01-01 22:45:56 +0200

    initial commit

---
+++ GetFix/GetFix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GetFix
+//
+//  Created by Ahmed Amin on 01/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GetFix/GetFix/GetFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetFixApp.swift
+//  GetFix
+//
+//  Created by Ahmed Amin on 01/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct GetFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
